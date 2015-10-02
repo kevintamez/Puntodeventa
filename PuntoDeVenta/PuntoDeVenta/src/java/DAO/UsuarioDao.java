4034d2d572bdd2ba39d6636c36fbbe9ee76190b3
kevintamez <kevintamez82@gmail.com>
2015-10-02 18:10:50 -0500

    nuestro projecto de PAPW Y BDM
    Aquí te lo mando

---
+++ PuntoDeVenta/PuntoDeVenta/src/java/DAO/UsuarioDao.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import Model.Usuario;
+import java.sql.Connection;
+
+/**
+ *
+ * @author kevin
+ */
+public class UsuarioDao {
+    public static void insertar(Usuario U){
+        
+    }
+}
---
+++ PuntoDeVenta/PuntoDeVenta/src/java/Model/Usuario.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author kevin
+ */
+public class Usuario {
+    int idUsuario;
+    String nombreUsuario;
+    String apellidoMterno;
+    String apellidoPaterno;
+    String CURP;
+    String colonia;
+    String calle;
+    String Estado;
+   
+    
+}
