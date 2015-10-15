d64f68cd807bfd30931b20f6783d4a8021124625
Ray-Var <ray.m_var@hotmail.com>
2015-10-14 19:04:38 -0500

    Modelo Sucursal
    Se agrego el modelo para la sucursal

---
+++ PuntoDeVenta/PuntoDeVenta/src/java/Model/Sucursal.java
@@ -0,0 +1,119 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author ray
+ */
+public class Sucursal {
+    int idSucursal;
+    String nombreSucursal;
+    int numeroExtSucursal;
+    String calleSucursal;
+    String coloniaSucursal;
+    int codigoPostalSucursal;
+    int idMunicipio;
+    int numeroSucursal;
+    int idEmpresa;
+
+    public Sucursal(int idSucursal, String nombreSucursal, int numeroExtSucursal, String calleSucursal, String coloniaSucursal, int codigoPostalSucursal, int idMunicipio, int numeroSucursal, int idEmpresa) {
+        this.idSucursal = idSucursal;
+        this.nombreSucursal = nombreSucursal;
+        this.numeroExtSucursal = numeroExtSucursal;
+        this.calleSucursal = calleSucursal;
+        this.coloniaSucursal = coloniaSucursal;
+        this.codigoPostalSucursal = codigoPostalSucursal;
+        this.idMunicipio = idMunicipio;
+        this.numeroSucursal = numeroSucursal;
+        this.idEmpresa = idEmpresa;
+    }
+
+    public Sucursal(String nombreSucursal, int numeroExtSucursal, String calleSucursal, String coloniaSucursal, int codigoPostalSucursal, int idMunicipio, int numeroSucursal, int idEmpresa) {
+        this.nombreSucursal = nombreSucursal;
+        this.numeroExtSucursal = numeroExtSucursal;
+        this.calleSucursal = calleSucursal;
+        this.coloniaSucursal = coloniaSucursal;
+        this.codigoPostalSucursal = codigoPostalSucursal;
+        this.idMunicipio = idMunicipio;
+        this.numeroSucursal = numeroSucursal;
+        this.idEmpresa = idEmpresa;
+    }
+
+    public int getIdSucursal() {
+        return idSucursal;
+    }
+
+    public void setIdSucursal(int idSucursal) {
+        this.idSucursal = idSucursal;
+    }
+
+    public String getNombreSucursal() {
+        return nombreSucursal;
+    }
+
+    public void setNombreSucursal(String nombreSucursal) {
+        this.nombreSucursal = nombreSucursal;
+    }
+
+    public int getNumeroExtSucursal() {
+        return numeroExtSucursal;
+    }
+
+    public void setNumeroExtSucursal(int numeroExtSucursal) {
+        this.numeroExtSucursal = numeroExtSucursal;
+    }
+
+    public String getCalleSucursal() {
+        return calleSucursal;
+    }
+
+    public void setCalleSucursal(String calleSucursal) {
+        this.calleSucursal = calleSucursal;
+    }
+
+    public String getColoniaSucursal() {
+        return coloniaSucursal;
+    }
+
+    public void setColoniaSucursal(String coloniaSucursal) {
+        this.coloniaSucursal = coloniaSucursal;
+    }
+
+    public int getCodigoPostalSucursal() {
+        return codigoPostalSucursal;
+    }
+
+    public void setCodigoPostalSucursal(int codigoPostalSucursal) {
+        this.codigoPostalSucursal = codigoPostalSucursal;
+    }
+
+    public int getIdMunicipio() {
+        return idMunicipio;
+    }
+
+    public void setIdMunicipio(int idMunicipio) {
+        this.idMunicipio = idMunicipio;
+    }
+
+    public int getNumeroSucursal() {
+        return numeroSucursal;
+    }
+
+    public void setNumeroSucursal(int numeroSucursal) {
+        this.numeroSucursal = numeroSucursal;
+    }
+
+    public int getIdEmpresa() {
+        return idEmpresa;
+    }
+
+    public void setIdEmpresa(int idEmpresa) {
+        this.idEmpresa = idEmpresa;
+    }
+    
+    
+}
